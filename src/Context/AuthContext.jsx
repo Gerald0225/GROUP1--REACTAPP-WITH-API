ea0ae7ec4b55dc1979fcdec6fd8878a289408ee5
Gerald Prio <geraldprio84@gmail.com>
2025-05-17 09:46:14 +0800

    Add files via upload

---
+++ src/Context/AuthContext.jsx
@@ -0,0 +1,30 @@
+import { createContext, useContext, useEffect, useState } from "react";
+
+const AuthContext = createContext();
+
+export const AuthProvider = ({ children }) => {
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+
+  useEffect(() => {
+    const auth = localStorage.getItem("auth");
+    if (auth === "true") setIsAuthenticated(true);
+  }, []);
+
+  const login = () => {
+    localStorage.setItem("auth", "true");
+    setIsAuthenticated(true);
+  };
+
+  const logout = () => {
+    localStorage.removeItem("auth");
+    setIsAuthenticated(false);
+  };
+
+  return (
+    <AuthContext.Provider value={{ isAuthenticated, login, logout }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
+
+export const useAuth = () => useContext(AuthContext);
